18482a9372f495e91cfc4173f505194363f29d38
loross14 <loross@umich.edu>
2023-04-04 20:41:56 -0400

    added and tested convert and uri function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MyToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
-        _disableInitializers();
     }
 
     function initialize() initializer public {
@@ -20,6 +19,22 @@ contract MyToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, Pausa
         __Pausable_init();
         __ERC1155Burnable_init();
         __UUPSUpgradeable_init();
+    }
+
+    function uri(uint256 ID) public view virtual override returns (string memory) {
+        return string(abi.encodePacked(super.uri(ID), Strings.toString(ID)));
+    }
+
+    function convert(address account, uint256 curTokenID, uint256 newTokenID, uint256 amount) 
+        public 
+        onlyOwner
+    {
+        //balance check, person has the token
+        require(balanceOf(account, curTokenID) >= amount);
+
+        //burn and mint
+        _burn(account, curTokenID, amount);
+        _mint(account, newTokenID, amount, "");
     }
 
     function setURI(string memory newuri) public onlyOwner {
@@ -46,14 +61,6 @@ contract MyToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, Pausa
         onlyOwner
     {
         _mintBatch(to, ids, amounts, data);
-    }
-
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
     function _authorizeUpgrade(address newImplementation)
--- scripts/deploy_with_ethers.ts
+++ scripts/deploy_with_ethers.ts
@@ -4,6 +4,7 @@ import { deploy } from './ethers-lib'
     try {
         const result = await deploy('MyToken', [])
         console.log(`address: ${result.address}`)
+        // await result.initialize()
     } catch (e) {
         console.log(e.message)
     }
